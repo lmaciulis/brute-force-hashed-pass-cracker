ca7a22642983036a8d3ce9d8efe7ceb7f6ef5505
Linas Maciulis <lmaciulis@gmail.com>
2021-03-21 17:26:04 +0200

    feature: prepare iterator code for goroutines

--- internal/decode/iterator.go
+++ internal/decode/iterator.go
@@ -12,14 +12,22 @@ type Iterator struct {
 	maxPassLen int
 }
 
-var hash []byte
+const (
+	iterableRunesStarIndex = 1
+)
+
+var (
+	hash []byte
+)
 
 func (i *Iterator) Run(decHash []byte) (pass string, err error) {
-	hash = decHash
+	hash = decHash // set as global var
 
-	for hLen := 1; hLen <= i.maxPassLen; hLen++ {
-		holder := char.NewHolder(hLen, i.getFirstChar())
-		pass, err = i.iterateHolder(holder)
+	//chFound := make(chan bool)
+	//chRes := make(chan string)
+
+	for _, h := range i.createHolders() {
+		pass, err = i.iterateHolder(h)
 
 		if err == nil {
 			return pass, nil
@@ -30,8 +38,10 @@ func (i *Iterator) Run(decHash []byte) (pass string, err error) {
 }
 
 func (i *Iterator) iterateHolder(holder *char.Holder) (pass string, err error) {
-	for hIdx := 0; hIdx < holder.GetLen(); hIdx++ {
-		pass, err = i.iterateHoldersRune(holder, hIdx)
+	// @todo implement channel
+	// @todo get/add starting character postpone
+	for hIdx := iterableRunesStarIndex; hIdx < holder.GetLen(); hIdx++ {
+		pass, err = i.iterateHolderRune(holder, hIdx)
 
 		if err == nil {
 			return pass, nil
@@ -41,18 +51,18 @@ func (i *Iterator) iterateHolder(holder *char.Holder) (pass string, err error) {
 	return "", ErrHashWasNotDecoded
 }
 
-func (i Iterator) iterateHoldersRune(holder *char.Holder, idx int) (pass string, err error) {
-	isLastIteration := holder.GetLen() == idx+1
+func (i Iterator) iterateHolderRune(holder *char.Holder, hIdx int) (pass string, err error) {
+	isLastIteration := holder.GetLen() == hIdx+1
 
 	for charIdx := 0; charIdx < i.charLen; charIdx++ {
-		holder.Set(idx, i.getChar(charIdx))
+		holder.Set(hIdx, i.getChar(charIdx))
 
 		if i.encoder.Match(holder.ToBytes(), hash) {
 			return holder.ToString(), nil
 		}
 
 		if isLastIteration == false {
-			pass, err = i.iterateHoldersRune(holder, idx+1)
+			pass, err = i.iterateHolderRune(holder, hIdx+1)
 
 			if err == nil {
 				return pass, nil
@@ -63,12 +73,27 @@ func (i Iterator) iterateHoldersRune(holder *char.Holder, idx int) (pass string,
 	return "", ErrHashWasNotDecoded
 }
 
-func (i *Iterator) getChar(idx int) rune {
-	return i.charList[idx]
+func (i *Iterator) createHolders() []*char.Holder {
+	var holders []*char.Holder
+	initChar := i.charList[0]
+
+	// Creates all possible lengths char holders, with every possible starting character.
+	// This allows efficiently split decoding jobs to separate go routines.
+	// One characters holder per one go routine.
+	for hLen := 1; hLen <= i.maxPassLen; hLen++ {
+		for charIdx := 0; charIdx < i.charLen; charIdx++ {
+			h := char.NewHolder(hLen, initChar)
+			h.Set(0, i.getChar(charIdx))
+
+			holders = append(holders, h)
+		}
+	}
+
+	return holders
 }
 
-func (i *Iterator) getFirstChar() rune {
-	return i.charList[0]
+func (i *Iterator) getChar(idx int) rune {
+	return i.charList[idx]
 }
 
 func NewIterator(encoder encode.Encoder) *Iterator {
